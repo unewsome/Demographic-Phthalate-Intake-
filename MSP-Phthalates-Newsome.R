15f81051387f51df4592ade4285ada0bc184285c
unewsome <umwoodard@gmail.com>
2019-01-08 21:31:55 -0600

    End of 010819

--- MSP-Phthalates-Newsome.R
+++ MSP-Phthalates-Newsome.R
@@ -25,5 +25,38 @@ pht_demo_bmi_cbq_diq<-merge(pht_demo_bmi_cbq,DIQ1516,all.x=FALSE)
 pht_demo_bmi_cbq_diq_inq<-merge(pht_demo_bmi_cbq_diq,INQ1516,all.x=FALSE)
 pht_all<-merge(pht_demo_bmi_cbq_diq_inq,MCQ1516,all.x=FALSE)
 
-pht_all%>%
-  select()
+pht_all2<-pht_all%>%
+  select(1:34,37,38,40,41,90,106,107,111,113,114,116:120,173,186,191,208,210,220,226,228,230,234,235,248,250,258)
+
+total_pht<-pht_all2%>%
+  mutate(total_pht=URXCNP+URXCOP+URXECP+URXMBP+URXMC1+URXMEP+URXMHP+URXMNP+URXMOH+URXMZP+URXMIB+URXHIBP+URXMCOH+URXMHBP+URXMHNC)
+
+demo_race<-total_pht%>%
+  group_by(RIDRETH1,RIDRETH3)%>%
+  summarise(mean_pht=mean(total_pht,na.rm=TRUE))
+  # mutate(Race_Ethncity=["Mexican American","Hispanic","Non-Hispanic White", "Non-Hispanic Black", "Non-Hispanic Asian", "Non-Hispanic Multi-racial"])
+  
+demo_race$Race_Ethnicity<-c("Mexican American","Hispanic","Non-Hispanic White", "Non-Hispanic Black", "Non-Hispanic Asian", "Non-Hispanic Multi-racial")
+
+
+ggplot(demo_race, aes(x=Race_Ethnicity,y=mean_pht))+
+  geom_col()+ xlab('Race/Ethnicity') +
+  ylab('Mean Phthalate Metabolite in Urine (ng/mL') +
+  ggtitle('           Comparison of Phthalate Metabolite Excretion by Race/Ethnicity') +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+demo_race_age<-total_pht%>%
+  group_by(RIDRETH1,RIDRETH3,RIAGENDR)%>%
+  summarise(mean_pht=mean(total_pht,na.rm=TRUE))
+
+
+demo_race_age$Race_Ethnicity_Gender<-c("Mexican American Male","Mexican American Female","Hispanic Male","Hispanic Female", "Non-Hispanic White Male", "Non-Hispanic White Female" , "Non-Hispanic Black Male",  "Non-Hispanic Black Female", "Non-Hispanic Asian Male","Non-Hispanic Asian Female", "Non-Hispanic Multi-racial Male", "Non-Hispanic Multi-racial Female")
+
+
+ggplot(demo_race_age, aes(x=Race_Ethnicity_Gender,y=mean_pht, fill=RIAGENDR))+
+  geom_col()+ xlab('Race/Ethnicity and Gender') +
+  ylab('Mean Phthalate Metabolite in Urine (ng/mL') +
+  ggtitle('           Comparison of Phthalate Metabolite Excretion by Race/Ethnicity and Gender') +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
